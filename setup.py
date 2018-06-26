d3329368b4eddf4f6f0366044efb3e7b93b7c28c
Julien Marrec <julien.marrec@gmail.com>
2018-06-26 12:00:03 +0200

    Configure tox correctly + include pep8 style check

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ from setuptools import Command, find_packages, setup
 from idftags import __version__
 
 
-this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
-    long_description = file.read()
+THIS_DIR = abspath(dirname(__file__))
+with open(join(THIS_DIR, 'README.rst'), encoding='utf-8') as file:
+    LONG_DESCRIPTION = file.read()
 
 
 class RunTests(Command):
@@ -33,15 +33,15 @@ class RunTests(Command):
 
 
 setup(
-    name = 'idf-tags',
-    version = __version__,
-    description = 'A ViM-usable tags file generator for EnergyPlus IDF files.',
-    long_description = long_description,
-    url = 'https://github.com/jmarrec/idf-tags',
-    author = 'Julien Marrec',
-    author_email = 'julien@effibem.com',
-    license = 'MIT',
-    classifiers = [
+    name='idf-tags',
+    version=__version__,
+    description='A ViM-usable tags file generator for EnergyPlus IDF files.',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/jmarrec/idf-tags',
+    author='Julien Marrec',
+    author_email='julien@effibem.com',
+    license='MIT',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: MIT License',
@@ -55,16 +55,16 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    keywords = 'energyplus,tags,vim,vi,cli',
-    packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['docopt'],
-    extras_require = {
-        'test': ['coverage', 'pytest', 'pytest-cov', 'eppy', 'tox'],
+    keywords='energyplus,tags,vim,vi,cli',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['eppy', 'docopt'],
+    extras_require={
+        'test': ['coverage', 'pytest', 'pytest-cov', 'pytest-pep8', 'tox'],
     },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'idf-tags=idftags.cli:main',
         ],
     },
-    cmdclass = {'test': RunTests},
+    cmdclass={'test': RunTests},
 )
